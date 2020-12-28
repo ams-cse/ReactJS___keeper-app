0704c8b491e858a051fc5f9b4ca9c87c9496bb60
Ahesanul Mahbub Shovon <shovonbd321@gmail.com>
2020-12-28 18:20:19 +0600

    First Version

---
+++ src/notes.jsx
@@ -0,0 +1,29 @@
+const notes = [
+    {
+        key: 1,
+        title : 'What is Lorem Ipsum?',
+        content : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+        key: 2,
+        title : 'Why do we use it?',
+        content : 'It is a long established fact that a reader will be distracted by the readable content of a page.'
+    },
+    {
+        key: 3,
+        title : 'Where does it come from?',
+        content : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.'
+    },
+    {
+        key: 4,
+        title : 'Where can I get some?',
+        content : 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.'
+    },
+    {
+        key: 5,
+        title : 'What is Lorem Ipsum?',
+        content : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    }
+
+];
+export default notes;
