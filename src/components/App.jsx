3660959815b21d377e2c34b6ce51173546630674
Ahesanul Mahbub Shovon <shovonbd321@gmail.com>
2020-12-31 19:31:59 +0600

    Final Version Kapper app adding few effects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
-    const [notes, setNote] = useState([]);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
 
-    function addNote(newNote) {
-        setNote(prevNote => {
-           return [...prevNote, newNote];
-        });
-    }
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-    function deleteNote(id) {
-        setNote(prevNote => {
-            return prevNote.filter((noteItem, index) => {
-                return index !== id;
-            });
-        });
-    }
-    
   return (
     <div>
       <Header />
-      <CreateArea 
-        onAdd={addNote}
-      />
-
-        {notes.map((newItem, index) => {
-            return < Note
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
             key={index}
             id={index}
-            title = {newItem.title}
-            content = {newItem.content}
-            onDelete = {deleteNote}
-            />
-        })}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
